62dc6165ef809c2cf659c9aa9a95c5f51b68c349
Tyler Cloetingh <tcloetingh@gmail.com>
2020-04-10 08:08:18 -0400

    stream action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,11 @@
 import streams from "../apis/stream";
-import { CREATE_STREAM } from "./types";
+import {
+  CREATE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
 
 export const signIn = userId => {
   return {
@@ -20,6 +26,30 @@ export const createStream = formValues => {
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
   };
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
 
 // This is only imported in the component and connected to mapPropsToState
