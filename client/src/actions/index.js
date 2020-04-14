0eb04e89d63aaa9f4779508c308c619af914066c
Tyler Cloetingh <tcloetingh@gmail.com>
2020-04-14 15:08:50 -0400

    render streams & create/delete conditional

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -21,8 +21,9 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => {
-  return async dispatch => {
-    const response = await streams.post("/streams", formValues);
+  return async (dispatch, getState) => {
+    const { userId } = getState().authState;
+    const response = await streams.post("/streams", { ...formValues, userId });
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
   };
